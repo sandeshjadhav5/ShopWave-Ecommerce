b35859c8d653e7d197bad9215c1ef246fc472b56
sandeshjadhav5 <jadhavs932@gmail.com>
2022-11-13 01:18:18 +0530

    added day5 work

--- nykaa-clone/src/Routes/AllRoutes.jsx
+++ nykaa-clone/src/Routes/AllRoutes.jsx
@@ -2,12 +2,14 @@ import { Route,Routes } from "react-router-dom";
 import Login from "../Components/Login"
 import Home from "../Components/Home/Home"
 import Signup from "../Components/Signup";
+import Products from "../Components/Products/Products";
 function AllRoutes(){
   return (
     <Routes>
       <Route path="/" element={<Home/>}/>
 <Route path="/login" element={<Login/>}/>
 <Route path="/signup" element={<Signup/>} />
+<Route path="/products" element={<Products/>}/>
     </Routes>
   )
 }
